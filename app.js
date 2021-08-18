8901c1e7b1d0e929c06ed430cb5df337be8c3665
Niaz Morshed <niazmorshed2007@gmail.com>
2021-08-18 10:13:06 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var myAnimation = new hoverEffect({
+  parent: document.querySelector(".wrapper"),
+  intensity: 0.3,
+  image1:
+    "https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  image2:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLgdL7upvRaRIdqAJgcy6RNftUZ_yiZ2ec0Q&usqp=CAU",
+  displacementImage:
+    "https://media.istockphoto.com/photos/picturesque-morning-in-plitvice-national-park-colorful-spring-scene-picture-id1093110112?k=6&m=1093110112&s=612x612&w=0&h=uBH7Rj-Ew_ixjunRrD_U7alq2ZUPJ_5XgMpe9xO52QQ=",
+});
